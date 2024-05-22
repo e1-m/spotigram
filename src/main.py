33516f6ab6638ad97322c39f7eb2c5b872f72c82
Ivan Mihun <vsk76090@gmail.com>
2024-05-22 23:18:17 +0200

    changed conditions to make them more readable

--- src/main.py
+++ src/main.py
@@ -3,35 +3,33 @@ from typing import Optional
 
 from spotify import SpotifyClientManager
 from telegram import TelegramClientManager
-from telethon.errors.rpcerrorlist import FloodWaitError
+from telethon.errors.rpcerrorlist import FloodWaitError, AboutTooLongError
 
-from config import settings
 from src.schemas import Track
-from utils import get_listening_to_track_string
 
 
 async def main():
     telegram_client = TelegramClientManager()
-    await telegram_client.start()
     spotify_client = SpotifyClientManager()
+
+    await telegram_client.start()
 
     previous_track: Optional[Track] = None
     while True:
         try:
             current_track = spotify_client.get_current_track()
-            if not current_track:
+            if current_track:
+                if previous_track != current_track:
+                    await telegram_client.display_track(current_track)
+                    previous_track = current_track
+            else:
                 if previous_track:
                     await telegram_client.hide_track()
                     previous_track = None
-                continue
-
-            if previous_track != current_track:
-                await telegram_client.display_track(current_track)
-                previous_track = current_track
             await asyncio.sleep(1)
         except FloodWaitError as e:
-            await asyncio.sleep(5+e.seconds)
-        except Exception:
+            await asyncio.sleep(5 + e.seconds)
+        except AboutTooLongError:
             await telegram_client.hide_track()
 
 
