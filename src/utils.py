704846c430abf5bd554f36c4afe6b5e15d726d07
Ivan Mihun <vsk76090@gmail.com>
2024-05-22 18:21:46 +0200

    fixed wrong order of operands

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,6 @@ from src.config import settings
 
 
 def get_listening_to_track_string(track: Track) -> str:
-    string_without_link = f"Listening to {track.name} by {track.artists} on Spotify:\n"
-    string_with_link = string_without_link + track.link
-    return string_without_link if len(string_with_link) <= settings.BIO_CHAR_LIMIT else string_with_link
+    string_without_link = f"Listening to {track.name} by {track.artists} on Spotify"
+    string_with_link = string_without_link + ":\n" + track.link
+    return string_with_link if len(string_with_link) <= settings.BIO_CHAR_LIMIT else string_without_link
