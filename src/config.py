4be7fe9870af4df240a472d17c660196f88dcfe5
Ivan Mihun <vsk76090@gmail.com>
2024-05-23 22:40:21 +0200

    added comments and CHECK_TRACK_PERIOD

--- src/config.py
+++ src/config.py
@@ -3,16 +3,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-
+    # Telegram app API ID and hash
     API_ID: int
     API_HASH: str
+
+    # Spotify app Client ID and secret
     SPOTIFY_CLIENT_ID: str
     SPOTIFY_CLIENT_SECRET: str
+
+    # Telegram credentials
     PHONE: str
     PASSWORD: str
+
     DEFAULT_BIO: str
-    SPOTIFY_EMOJI_STATUS_ID: int
     DEFAULT_EMOJI_STATUS_ID: int
+    SPOTIFY_EMOJI_STATUS_ID: int
+
+    CHECK_TRACK_PERIOD: int
+
     SCOPE: str = 'user-read-playback-state user-read-currently-playing'
     REDIRECT_URL: str = 'http://localhost:8888/callback'
     BIO_CHAR_LIMIT: int = 140
