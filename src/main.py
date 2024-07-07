d4e86cc9a4469b1559fdcf9c5af87fff390957c6
Ivan <131366875+ivan-22-3-5@users.noreply.github.com>
2024-07-07 16:21:17 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -49,7 +49,8 @@ class TrackChangeMonitor:
                 await self.telegram_client.hide_track()
         await self.telegram_client.hide_track()
 
-#TODO: review code, make the code input possible 
+#TODO: review code, make the code input possible
+#TODO: make adding several accounts possible 
 async def main():
     track_change_monitor = TrackChangeMonitor()
 
