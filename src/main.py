3ffb0d2290caa543f2fc32e6f26f0ccf9a525880
Ivan Mihun <vsk76090@gmail.com>
2024-10-28 13:04:23 +0100

    refactor: refactor using events

--- src/main.py
+++ src/main.py
@@ -1,75 +1,37 @@
 import asyncio
 
-from requests.exceptions import RequestException
 from threading import Thread
-from typing import Optional
-from urllib.error import HTTPError
 
-from telethon.errors.rpcerrorlist import FloodWaitError
-from telethon.errors.rpcbaseerrors import RPCError
-
-from spotify import SpotifyClientManager
+from spotify import SpotifyMonitor
 from telegram import TelegramClientManager
 from schemas import Track
-from config import settings
-
-
-class TrackChangeMonitor:
-    def __init__(self):
-        self.telegram_client = TelegramClientManager()
-        self.spotify_client = SpotifyClientManager()
-        self.is_monitoring = False
-
-    async def start_monitoring(self):
-        await self.telegram_client.start()
-        self.is_monitoring = True
-        await self.monitor_track_change()
-
-    def stop_monitoring(self):
-        self.is_monitoring = False
-
-    async def monitor_track_change(self):
-        previous_track: Optional[Track] = None
-        while self.is_monitoring:
-            try:
-                current_track = self.spotify_client.get_current_track()
-                if current_track:
-                    if previous_track != current_track:
-                        await self.telegram_client.display_track(current_track)
-                        previous_track = current_track
-                else:
-                    if previous_track:
-                        await self.telegram_client.hide_track()
-                        previous_track = None
-                await asyncio.sleep(settings.CHECK_TRACK_PERIOD)
-            except FloodWaitError as e:
-                await asyncio.sleep(settings.CHECK_TRACK_PERIOD + e.seconds)
-            except (RPCError, HTTPError, RequestException):
-                await self.telegram_client.hide_track()
-        await self.telegram_client.hide_track()
-
-
-if settings.USE_TRAY:
-    import pystray
-    from PIL import Image
-
-    async def main():
-        track_change_monitor = TrackChangeMonitor()
-
-        icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"),
-                            menu=pystray.Menu(pystray.MenuItem('Quit', lambda: track_change_monitor.stop_monitoring())))
-        Thread(target=icon.run).start()
-
-        print("The app has started")
-        await track_change_monitor.start_monitoring()
-
-        icon.stop()
-else:
-    async def main():
-        track_change_monitor = TrackChangeMonitor()
-        print("The app has started without tray")
-        await track_change_monitor.start_monitoring()
-
+import pystray
+from PIL import Image
 
 if __name__ == '__main__':
+    spotify_monitor = SpotifyMonitor()
+
+    telegram_client = TelegramClientManager()
+
+
+    @spotify_monitor.on_track_change
+    async def on_track_change(track: Track):
+        await telegram_client.display_track(track)
+
+
+    @spotify_monitor.on_playback_end
+    async def on_playback_end():
+        await telegram_client.hide_track()
+
+
+    async def main():
+        await telegram_client.start()
+
+        icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"),
+                            menu=pystray.Menu(pystray.MenuItem('Quit', lambda: spotify_monitor.stop_monitoring())))
+        Thread(target=icon.run).start()
+        print("The app has started")
+        await spotify_monitor.start_monitoring()
+        icon.stop()
+
     asyncio.run(main())
--- src/spotify.py
+++ src/spotify.py
@@ -1,8 +1,14 @@
+import asyncio
 import os
-from typing import Optional
+from typing import Optional, Callable, Coroutine
+from urllib.error import HTTPError
+from requests.exceptions import RequestException
 
+from telethon.errors.rpcerrorlist import FloodWaitError
+from telethon.errors.rpcbaseerrors import RPCError
 from spotipy import SpotifyOAuth, Spotify, CacheFileHandler
 from spotipy.exceptions import SpotifyException
+from events import Events
 
 from config import settings
 from schemas import Track
@@ -31,3 +37,43 @@ class SpotifyClientManager:
                     return Track(name=name, artists=artists, link=link)
         except SpotifyException:
             return None
+
+
+class SpotifyMonitor:
+    def __init__(self):
+        self.events = Events('on_track_change', 'on_playback_end')
+        self.spotify_client = SpotifyClientManager()
+        self.is_monitoring = False
+
+    async def start_monitoring(self):
+        self.is_monitoring = True
+        await self._monitor_playback()
+
+    def stop_monitoring(self):
+        self.is_monitoring = False
+
+    async def _monitor_playback(self):
+        previous_track: Optional[Track] = None
+        while self.is_monitoring:
+            try:
+                current_track = self.spotify_client.get_current_track()
+                if current_track:
+                    if previous_track != current_track:
+                        await self.events.on_track_change(current_track)
+                        previous_track = current_track
+                else:
+                    if previous_track:
+                        await self.events.on_playback_end()
+                        previous_track = None
+                await asyncio.sleep(settings.CHECK_TRACK_PERIOD)
+            except FloodWaitError as e:
+                await asyncio.sleep(settings.CHECK_TRACK_PERIOD + e.seconds)
+            except (RPCError, HTTPError, RequestException):
+                await self.events.on_playback_end()
+        await self.events.on_playback_end()
+
+    def on_track_change(self, func: Callable[[Track], Coroutine[None, None, None]]):
+        self.events.on_track_change = func
+
+    def on_playback_end(self, func: Callable[[], Coroutine[None, None, None]]):
+        self.events.on_playback_end = func
