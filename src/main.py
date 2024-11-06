a6bfb889fa92dd044777b7abb2346342ba703f3e
Ivan Mihun <vsk76090@gmail.com>
2024-11-06 13:20:28 +0100

    refactor: move everything needed to start polling to the main function

--- src/main.py
+++ src/main.py
@@ -9,30 +9,26 @@ from telegram import TelegramClientManager
 from schemas import Track
 
 
-if __name__ == '__main__':
+async def main():
     spotify_monitor = SpotifyMonitor()
-
     telegram_client = TelegramClientManager()
-
+    await telegram_client.start()
 
     @spotify_monitor.on_track_change
     async def display_track(track: Track):
         await telegram_client.display_track(track)
 
-
     @spotify_monitor.on_playback_end
     async def hide_track():
         await telegram_client.hide_track()
 
+    icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"),
+                        menu=pystray.Menu(pystray.MenuItem('Quit', lambda: spotify_monitor.stop_monitoring())))
+    Thread(target=icon.run).start()
+    print("The app has started")
+    await spotify_monitor.start_monitoring()
+    icon.stop()
 
-    async def main():
-        await telegram_client.start()
 
-        icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"),
-                            menu=pystray.Menu(pystray.MenuItem('Quit', lambda: spotify_monitor.stop_monitoring())))
-        Thread(target=icon.run).start()
-        print("The app has started")
-        await spotify_monitor.start_monitoring()
-        icon.stop()
-
+if __name__ == '__main__':
     asyncio.run(main())
