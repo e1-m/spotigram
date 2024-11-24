4b825352f55ebfd70ba773689127ec4f8373c87a
Ivan Mihun <vsk76090@gmail.com>
2024-11-24 14:33:18 +0100

    feat: stopping icon on tray action "quit"

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import asyncio
-
 from threading import Thread
+
 import pystray
 from PIL import Image
 
@@ -22,13 +22,15 @@ async def main():
     async def hide_track():
         await telegram_client.hide_track()
 
-    icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"),
-                        menu=pystray.Menu(pystray.MenuItem('Quit', lambda: spotify_monitor.stop_monitoring())))
-    Thread(target=icon.run).start()
     print("The app has started")
-    asyncio.create_task(telegram_client.monitor_bio_changes())
-    await spotify_monitor.start_monitoring()
-    icon.stop()
+    icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"))
+    icon.menu = pystray.Menu(pystray.MenuItem('Quit', lambda: spotify_monitor.stop_monitoring() or icon.stop()))
+    Thread(target=icon.run).start()
+    await asyncio.wait(
+        [asyncio.create_task(spotify_monitor.start_monitoring()),
+         asyncio.create_task(telegram_client.monitor_bio_changes())],
+        return_when=asyncio.FIRST_COMPLETED
+    )
 
 
 if __name__ == '__main__':
