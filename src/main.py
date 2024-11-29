b1dd96e9c06adffec90bba6311b108d67ba4ca67
Ivan Mihun <vsk76090@gmail.com>
2024-11-29 14:11:23 +0100

    feat: setting bio and emoji status to defaults when shutting down

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import signal
 from threading import Thread
 
 import pystray
@@ -8,26 +9,38 @@ from spotify import SpotifyMonitor
 from telegram import TelegramClientManager
 from track import Track
 
+spotify_monitor = SpotifyMonitor()
+telegram_client = TelegramClientManager()
+
+
+@spotify_monitor.on_track_change
+async def display_track(track: Track):
+    await telegram_client.display_track(track)
+
+
+@spotify_monitor.on_playback_end
+async def hide_track():
+    await telegram_client.hide_track()
+
+
+def clean_up(signum, frame):
+    spotify_monitor.stop_monitoring()
+    telegram_client.stop_monitoring()
+
+
+signal.signal(signal.SIGINT, clean_up)
+signal.signal(signal.SIGTERM, clean_up)
+
 
 async def main():
-    spotify_monitor = SpotifyMonitor()
-    telegram_client = TelegramClientManager()
     await telegram_client.connect()
-
-    @spotify_monitor.on_track_change
-    async def display_track(track: Track):
-        await telegram_client.display_track(track)
-
-    @spotify_monitor.on_playback_end
-    async def hide_track():
-        await telegram_client.hide_track()
 
     print("The app has started")
     icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"))
     icon.menu = pystray.Menu(pystray.MenuItem('Quit', lambda: (spotify_monitor.stop_monitoring() or
                                                                telegram_client.stop_monitoring() or
                                                                icon.stop())))
-    Thread(target=icon.run).start()
+    Thread(target=icon.run, daemon=True).start()
     await asyncio.gather(spotify_monitor.start_monitoring(), telegram_client.start_monitoring())
 
 
