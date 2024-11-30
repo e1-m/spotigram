4c239decc4c0f455ae43435f8ac7e9c89fe386ee
Ivan Mihun <vsk76090@gmail.com>
2024-11-30 14:35:35 +0100

    feat: pausing and resuming monitoring

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import asyncio
 import signal
-from threading import Thread
+from threading import Thread, Lock
 
 import pystray
 from PIL import Image
 
-from spotify import SpotifyMonitor
-from telegram import TelegramClientManager
+from spotify import SpotifyMonitor, SpotifyMonitoringException
+from telegram import TelegramClientManager, TelegramMonitoringException
 from track import Track
 
 spotify_monitor = SpotifyMonitor()
@@ -24,8 +24,15 @@ async def hide_track():
 
 
 def clean_up(signum, frame):
-    spotify_monitor.stop_monitoring()
-    telegram_client.stop_monitoring()
+    try_stop_monitoring()
+
+
+def try_stop_monitoring():
+    try:
+        spotify_monitor.stop_monitoring()
+        telegram_client.stop_monitoring()
+    except (SpotifyMonitoringException, TelegramMonitoringException):
+        pass
 
 
 signal.signal(signal.SIGINT, clean_up)
@@ -36,12 +43,28 @@ async def main():
     await telegram_client.connect()
 
     print("The app has started")
-    icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"))
-    icon.menu = pystray.Menu(pystray.MenuItem('Quit', lambda: (spotify_monitor.stop_monitoring() or
-                                                               telegram_client.stop_monitoring() or
-                                                               icon.stop())))
-    Thread(target=icon.run, daemon=True).start()
-    await asyncio.gather(spotify_monitor.start_monitoring(), telegram_client.start_monitoring())
+    paused = Lock()
+
+    def pause():
+        nonlocal paused
+        paused.acquire(blocking=True)
+        try_stop_monitoring()
+
+    while True:
+        icon = pystray.Icon("spotigram", Image.open("icons/spotigram.png"))
+        icon.menu = pystray.Menu(pystray.MenuItem('Quit', lambda icn: try_stop_monitoring() or icn.stop()),
+                                 pystray.MenuItem('Resume', lambda icn: paused.release() or icn.stop())
+                                 if paused.locked() else
+                                 pystray.MenuItem('Pause', lambda icn: pause() or icn.stop()))
+        Thread(target=icon.run, daemon=True).start()
+        if paused.locked():
+            paused.acquire(blocking=True)
+            paused.release()
+            continue
+        await asyncio.gather(spotify_monitor.start_monitoring(), telegram_client.start_monitoring())
+        # if monitoring is finished, but not paused, exit
+        if not paused.locked():
+            break
 
 
 if __name__ == '__main__':
